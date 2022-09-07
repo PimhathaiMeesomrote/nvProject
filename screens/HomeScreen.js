8fbcf408068281f585eb26ac25d35cfc58fa6b3b
PimhathaiMeesomrote <pmeesomrote@gmail.com>
2022-09-07 10:20:08 +0700

    7/09

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,37 @@ import { View, Text, Button } from 'react-native'
 import React from 'react'
 
 import { Ionicons } from '@expo/vector-icons';
+import {
+    HeaderButtons,
+    HeaderButton,
+    Item,
+    HiddenItem,
+    OverflowMenu,
+  } from 'react-navigation-header-buttons';
+
+  const IoniconsHeaderButton = (props) => (
+    // the `props` here come from <Item ... />
+    // you may access them and pass something else to `HeaderButton` if you like
+    <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
+  );
+  
+  const ReusableItem = ({ onPress }) => <Item title="Edit" onPress={onPress} />;
+  
+  const ReusableHiddenItem = ({ onPress }) => <HiddenItem title="hidden2" onPress={onPress} />;
 
 const HomeScreen = ({ navigation }) => {
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+          // in your app, you can extract the arrow function into a separate component
+          // to avoid creating a new one every time you update the options
+          headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+              <Item title="register" iconName="person-add" onPress={() => alert('search')} />
+              
+            </HeaderButtons>
+          ),
+        });
+      }, [navigation]);
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Ionicons name='home' size={30} color='#008b8b'/>
